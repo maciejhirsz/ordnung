16ec20d7d2f63499f4aac33944fc4dce9c8ffd88
Maciej Hirsz <hello@maciej.codes>
2020-04-21 11:27:09 +0200

    Removed sentinels

--- src/lib.rs
+++ src/lib.rs
@@ -57,8 +57,9 @@ struct Node<K, V> {
     // Hash of the key
     pub hash: u64,
 
-    // Value stored.
-    pub value: V,
+    // Value stored. We'll use `None` as a sentinel value for removed
+    // entries.
+    pub value: Option<V>,
 
     // Store vector index pointing to the `Node` for which `hash` is smaller
     // than that of this `Node`.
@@ -98,7 +99,7 @@ impl<K, V> Node<K, V> {
         Node {
             key,
             hash,
-            value,
+            value: Some(value),
             left: Cell::new(None),
             right: Cell::new(None),
         }
@@ -170,9 +171,7 @@ where
 
         match self.find(&key, hash) {
             Hit(idx) => unsafe {
-                let slot = &mut self.store.get_unchecked_mut(idx).value;
-
-                Some(core::mem::replace(slot, value))
+                self.store.get_unchecked_mut(idx).value.replace(value)
             },
             Miss(parent) => {
                 if let Some(parent) = parent {
@@ -209,7 +208,43 @@ where
         let hash = hash_key(key);
 
         match self.find(key, hash) {
-            Hit(idx) => Some(unsafe { &self.store.get_unchecked(idx).value }),
+            Hit(idx) => {
+                let node = unsafe { self.store.get_unchecked(idx) };
+
+                node.value.as_ref()
+            },
+            Miss(_) => None,
+        }
+    }
+
+    /// Returns a mutable reference to the value corresponding to the key.
+    ///
+    /// The key may be any borrowed form of the map's key type, but Hash and Eq
+    /// on the borrowed form must match those for the key type.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use ordnung::Map;
+    ///
+    /// let mut map = Map::new();
+    /// map.insert(1, "a");
+    /// if let Some(x) = map.get_mut(&1) {
+    ///     *x = "b";
+    /// }
+    /// assert_eq!(map[&1], "b");
+    /// ```
+    pub fn get_mut<Q>(&mut self, key: &Q) -> Option<&mut V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let hash = hash_key(key);
+
+        match self.find(key, hash) {
+            Hit(idx) => unsafe {
+                self.store.get_unchecked_mut(idx).value.as_mut()
+            },
             Miss(_) => None,
         }
     }
@@ -242,36 +277,6 @@ where
         }
     }
 
-    /// Returns a mutable reference to the value corresponding to the key.
-    ///
-    /// The key may be any borrowed form of the map's key type, but Hash and Eq
-    /// on the borrowed form must match those for the key type.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use ordnung::Map;
-    ///
-    /// let mut map = Map::new();
-    /// map.insert(1, "a");
-    /// if let Some(x) = map.get_mut(&1) {
-    ///     *x = "b";
-    /// }
-    /// assert_eq!(map[&1], "b");
-    /// ```
-    pub fn get_mut<Q>(&mut self, key: &Q) -> Option<&mut V>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        let hash = hash_key(key);
-
-        match self.find(key, hash) {
-            Hit(idx) => Some(unsafe { &mut self.store.get_unchecked_mut(idx).value }),
-            Miss(_) => None,
-        }
-    }
-
     /// Get a mutable reference to entry at key. Inserts a new entry by
     /// calling `F` if absent.
     // TODO: Replace with entry API
@@ -283,7 +288,15 @@ where
         let hash = hash_key(&key);
 
         match self.find(&key, hash) {
-            Hit(idx) => &mut self.store[idx].value,
+            Hit(idx) => {
+                let node = unsafe { self.store.get_unchecked_mut(idx) };
+
+                if node.value.is_none() {
+                    node.value = Some(fill());
+                }
+
+                node.value.as_mut().unwrap()
+            },
             Miss(parent) => {
                 let idx = self.store.len();
 
@@ -293,7 +306,7 @@ where
 
                 self.store.push(Node::new(key, fill(), hash));
 
-                &mut self.store[idx].value
+                self.store[idx].value.as_mut().unwrap()
             },
         }
     }
@@ -321,33 +334,12 @@ where
     {
         let hash = hash_key(key);
 
-        let index = match self.find(key, hash) {
-            Hit(idx) => idx,
+        match self.find(key, hash) {
+            Hit(idx) => unsafe {
+                self.store.get_unchecked_mut(idx).value.take()
+            },
             Miss(_) => return None,
-        };
-
-        // Removing a node would screw the tree badly, it's easier to just
-        // recreate it.
-        let mut removed = None;
-        let capacity = self.store.len();
-        let old = mem::replace(&mut self.store, Vec::with_capacity(capacity));
-
-        for (i, Node { key, value, hash, .. }) in old.into_iter().enumerate() {
-            if i == index {
-                // Rust doesn't like us moving things from `node`, even if
-                // it is owned. Replace fixes that.
-                removed = Some(value);
-            } else {
-                // Faster than .insert() since we can avoid hashing
-                if let Miss(Some(parent)) = self.find(key.borrow(), hash) {
-                    parent.set(NonZeroU32::new(self.store.len() as u32));
-                }
-
-                self.store.push(Node::new(key, value, hash));
-            }
         }
-
-        removed
     }
 
     /// Returns the number of elements in the map.
@@ -565,14 +557,32 @@ impl<'i, K, V> Iterator for Iter<'i, K, V> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|node| (&node.key, &node.value))
+        while let Some(node) = self.inner.next() {
+            let value = match node.value {
+                Some(ref value) => value,
+                None => continue,
+            };
+
+            return Some((&node.key, value));
+        }
+
+        None
     }
 }
 
 impl<K, V> DoubleEndedIterator for Iter<'_, K, V> {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.inner.next_back().map(|node| (&node.key, &node.value))
+        while let Some(node) = self.inner.next_back() {
+            let value = match node.value {
+                Some(ref value) => value,
+                None => continue,
+            };
+
+            return Some((&node.key, value));
+        }
+
+        None
     }
 }
 
@@ -596,14 +606,32 @@ impl<'a, K, V> Iterator for IterMut<'a, K, V> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|node| (&node.key, &mut node.value))
+        while let Some(node) = self.inner.next() {
+            let value = match node.value {
+                Some(ref mut value) => value,
+                None => continue,
+            };
+
+            return Some((&node.key, value));
+        }
+
+        None
     }
 }
 
 impl<K, V> DoubleEndedIterator for IterMut<'_, K, V> {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.inner.next_back().map(|node| (&node.key, &mut node.value))
+        while let Some(node) = self.inner.next_back() {
+            let value = match node.value {
+                Some(ref mut value) => value,
+                None => continue,
+            };
+
+            return Some((&node.key, value));
+        }
+
+        None
     }
 }
 
